348d3c61e66631a9d1aa12572a75d03f9acf48d5
sangyeob yoo <vnvj0033@gmail.com>
2021-05-02 21:29:47 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import "./App.css";
 import ChildComponet from "./child.js";
 
 function App() {
+  const text = "오늘도 내일도 공부합시다 ㅜㅜ";
+  const sayHello = function () {
+    return <h3>오늘 너무 빡쌤</h3>;
+  };
+  const sayHello2 = function () {
+    alert("안녕하세요!");
+  };
   return (
     <div>
       <h1>안녕하세용!</h1>
+      <h2>{text}</h2>
+      {sayHello()}
+      <div onClick={sayHello2}>클릭해 주세요</div>
       <ChildComponet />
       <ChildComponet />
       <ChildComponet />
