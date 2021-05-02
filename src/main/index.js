d34ce84a90289f5041e532cc55657f2261e17f36
sangyeob yoo <vnvj0033@gmail.com>
2021-05-02 22:43:49 +0900

    used network for products

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,22 @@
 import "./index.css";
+import axios from "axios";
+import React from "react";
 
 function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://2d51ee40-739b-40a4-8408-61ede7afc58c.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.log("에러발생 ", error);
+      });
+  }, []);
   return (
     <div>
       <div id="header">
@@ -13,7 +29,28 @@ function MainPage() {
           <img src="images/banners/banner1.png" />
         </div>
         <h1>판매되는 상품들</h1>
-        <div id="product-list"></div>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div id="footer"></div>
     </div>
