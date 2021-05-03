4254a5b4272f98b2850ac16635d7918740b1f458
sangyeob yoo <vnvj0033@gmail.com>
2021-05-03 20:41:36 +0900

    add page and used react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPage from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPage />
+        </Route>
+        <Route exact={true} path="/product">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/product/index.js
@@ -0,0 +1,5 @@
+function ProductPage() {
+  return <h1>상품상세페이지</h1>;
+}
+
+export default ProductPage;
