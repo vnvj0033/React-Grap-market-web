a22c42f571d58e91439657c88e29dff1b5bbb714
sangyeob yoo <vnvj0033@gmail.com>
2021-05-08 19:45:39 +0900

    implement upload page

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,5 +1,24 @@
+import { Divider, Form, Input } from "antd";
+import "./index.css";
+
 function UploadPage() {
-  return <h1>업로드 화면</h1>;
+  const onSubmit = () => {};
+  return (
+    <div>
+      <Form name="상품 업로드" onFinish={onSubmit}>
+        <Form.Item name="upload">
+          <div id="upload-img-placeholder">
+            <img src="/images/icons/camera.png" />
+            <span>이미지를 업로드해주세요.</span>
+          </div>
+        </Form.Item>
+        <Divider />
+        <Form.Item>
+          <Input size="large" placeholder="이름을 입력해 주세요" />
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default UploadPage;
